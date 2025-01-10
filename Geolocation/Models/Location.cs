9a02e5be3d4f58abd06081d4b0e7fde434334f18
amaechijude <ugwuamaechi5@gmail.com>
2025-01-10 22:27:02 +0100

    Add project files.

---
+++ Geolocation/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Geolocation.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? LocationName { get; set; }
+        [Url]
+        public string? GoogleMapURL { get; set; }
+    }
+}
