267ebc480f7988bcd8622afc42ba467578a057f6
amaechijude <ugwuamaechi5@gmail.com>
2025-01-11 16:48:43 +0100

    reame

--- Geolocation/Controllers/HomeController.cs
+++ Geolocation/Controllers/HomeController.cs
@@ -24,11 +24,6 @@ namespace Geolocation.Controllers
             return View();
         }
 
-        public IActionResult Location()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
